9b1a1e888f4d853d9593608a2121832b2f768684
Munjie <892790756@qq.com>
2019-09-06 07:30:28 +0800

    add mp3

--- src/main/java/com/mwj/personweb/controller/HomeController.java
+++ src/main/java/com/mwj/personweb/controller/HomeController.java
@@ -14,10 +14,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.security.Principal;
 import java.util.List;
 
@@ -111,5 +115,47 @@ public class HomeController {
     }
     model.addAttribute("tags", tags);
     return pageUtil.forward(authentication, model, "front/tags");
+  }
+
+  @RequestMapping("/mp3")
+  public String getAudio(HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    String range = request.getHeader("Range");
+    String[] rs = range.split("\\=");
+    range = rs[1].split("\\-")[0];
+
+    String path = request.getServletContext().getRealPath("/");
+    File file = new File("/appdata/web/2019.mp3");
+    if (!file.exists()) throw new RuntimeException("音频文件不存在 --> 404");
+    OutputStream os = response.getOutputStream();
+    FileInputStream fis = new FileInputStream(file);
+    long length = file.length();
+    System.out.println("file length : " + length);
+    // 播放进度
+    int count = 0;
+    // 播放百分比
+    int percent = (int) (length * 0.4);
+
+    int irange = Integer.parseInt(range);
+    length = length - irange;
+
+    response.addHeader("Accept-Ranges", "bytes");
+    response.addHeader("Content-Length", length + "");
+    response.addHeader("Content-Range", "bytes " + range + "-" + length + "/" + length);
+    response.addHeader("Content-Type", "audio/mpeg;charset=UTF-8");
+
+    int len = 0;
+    byte[] b = new byte[1024];
+    while ((len = fis.read(b)) != -1) {
+      os.write(b, 0, len);
+      count += len;
+      if (count >= percent) {
+        break;
+      }
+    }
+    System.out.println("count: " + count + ", percent: " + percent);
+    fis.close();
+    os.close();
+    return null;
   }
 }
